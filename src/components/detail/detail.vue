90236abfec6d03f16e5cb7e878bccedb118be806
Steven Villegas Castellanos <steven.villegas@esub.com>
2021-11-09 13:07:04 -0500

    initial commit

---
+++ src/components/detail/detail.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="hello">
+    <h1>Detail {{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Detail',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,22 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHashHistory } from 'vue-router' // import VueRouter from 'vue-router'
 import App from './App.vue'
+import List from './components/list/list.vue'
+import Detail from './components/detail/detail.vue'
 
-createApp(App).mount('#app')
+// const NotFound = { template: '<p>Page not found</p>' }
+const routes = [
+  { path: '/', component: List },
+  { path: '/detail', component: Detail },
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+})
+
+const app = createApp(App)
+
+app.use(router)
+
+app.mount('#app')
